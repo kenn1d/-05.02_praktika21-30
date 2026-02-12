9724da67ed5db2caa231b8823b2109b15411b1c8
kenn1d <makarkibanov220@gmail.com>
2026-02-13 00:28:23 +0500

    Разработанный класс отправки сообщения на почту. Кибанов.

---
+++ Classes/SendMail.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace praktika21_30_.Classes
+{
+    public class SendMail
+    {
+        public static void SendMessage(string Message, string To)
+        {
+            // Содаём smtp клиент, в качестве хоста указываем яндекс
+            var smtpClient = new SmtpClient("smtp.yandex.ru")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential("yandex@yandex.ru", "password"),
+                EnableSsl = true
+            };
+            // Вызываем метод send, который отправляет письмо на указанный адрес
+            smtpClient.Send("landaxer@yandex.ru", To, "Проект Praktika21(30)", Message);
+        }
+    }
+}
